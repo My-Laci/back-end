78ff4afc000c09b46a26840204ea302c897daacf
Adelnors <erlando.putra.21@gmail.com>
2024-08-09 22:54:12 +0700

    feat: added reset password features

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,13 @@
 const bcrypt = require("bcrypt")
+const userService = require("../services/userService");
 
 exports.forgotPassword = async (req, res) => {
-
+    try {
+        const { email } = req.body;
+        if (!email) throw Error("An email is required.");
+        const createdPasswordResetOTP = await userService.sendPasswordResetOTP(email);
+        res.status(200).json(createdPasswordResetOTP);
+    } catch (error) {
+        res.statu(400).send(error.message);
+    }
 };
