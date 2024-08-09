932cc691cc00676d5ceee85220fe0b1b327d002a
Adelnors <erlando.putra.21@gmail.com>
2024-08-09 22:54:12 +0700

    feat: added reset password features

--- services/userService.js
+++ services/userService.js
@@ -1,11 +1,24 @@
 const User = require("../models/user");
+const { sendOTP, verifyOTP, deleteOTP } = require("./otpService");
 
-exports.forgotPassword = async (email) => {
+exports.sendPasswordResetOTP = async (email) => {
     try {
-        const existingUser = await User.findOne()
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw Error("There is no account existed with the provided email");
+        }
 
+        const otpDetails = {
+            email,
+            subject: "Password Reset",
+            message: "Enter the code below to reset your password.",
+            duration: 5,
+        }
+
+        const createdOTP = await sendOTP(otpDetails);
+        return createdOTP;
     } catch (error) {
-
+        throw error;
     }
 
 }
