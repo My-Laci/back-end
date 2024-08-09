532392a2b723afff80493cff861766cec902dd1b
Adelnors <erlando.putra.21@gmail.com>
2024-08-09 22:00:01 +0700

    feat: added user controls

---
+++ services/userService.js
@@ -0,0 +1,11 @@
+const User = require("../models/user");
+
+exports.forgotPassword = async (email) => {
+    try {
+        const existingUser = await User.findOne()
+
+    } catch (error) {
+
+    }
+
+}
