458113bb99c834a68d685582855dee0d99512b18
Adelnors <erlando.putra.21@gmail.com>
2024-08-12 13:22:26 +0700

    feat: added voucher implementation for registration

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -5,30 +5,30 @@ exports.sendVerificationOTP = async (req, res) => {
         const { email } = req.body;
         if (!email) throw Error("An email is required");
 
-        const createdEmailVerificationOTP = await emailVerifService.sendVerificationOTPEmail(email);
+        const createdEmailVerificationOTP = await emailVerifService.sendVerificationOTP(email);
         res.status(200).json(createdEmailVerificationOTP);
     } catch (error) {
         res.status(400).send(error.message);
     }
 };
 
-exports.sendVerificationOTPSignUp = async (email) => {
-    try {
-        if (!email) throw Error("An email is required");
+// exports.sendVerificationOTPSignUp = async (email) => {
+//     try {
+//         if (!email) throw Error("An email is required");
 
-        const createdEmailVerificationOTP = await emailVerifService.sendVerificationOTPSignUp(email);
-        return createdEmailVerificationOTP;
-    } catch (error) {
-        throw error; // Propagate the error so it can be caught and handled in authController.js
-    }
-};
+//         const createdEmailVerificationOTP = await emailVerifService.sendVerificationOTPSignUp(email);
+//         return createdEmailVerificationOTP;
+//     } catch (error) {
+//         throw error; // Propagate the error so it can be caught and handled in authController.js
+//     }
+// };
 
 exports.verifyOTP = async (req, res) => {
     try {
         let { email, otp } = req.body;
         if (!(email && otp)) throw Error("Empty OTP details are not allowed");
 
-        await emailVerifService.verifyOTPEmail({ email, otp });
+        await emailVerifService.verifyOTP({ email, otp });
         res.status(200).json({ email, verified: true });
     } catch (error) {
         res.status(400).send(error.message);
