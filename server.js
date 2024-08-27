536a20364fa417a8e80cbb6d1a7ae50045433879
Adelnors <erlando.putra.21@gmail.com>
2024-08-27 10:46:40 +0700

    fix: allow request from all origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 // Routes
 app.use("/", require("./routes/routes"));
