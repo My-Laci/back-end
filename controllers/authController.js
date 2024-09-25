904a33bfc0cbdd987c4c55edcd9420f58b1e67d9
Adelnors <erlando.putra.21@gmail.com>
2024-09-25 15:11:16 +0700

    fix: fix error message

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,7 +58,7 @@ exports.signIn = async (req, res) => {
         const authenticatedUser = await authService.signIn(req.body);
         res.status(200).json(authenticatedUser);
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(400).json({ message: error.message });
     }
 };
 
