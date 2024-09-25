f3a9a9ba0b6c14806411549bdf3d0e400935478e
Adelnors <erlando.putra.21@gmail.com>
2024-09-25 15:11:07 +0700

    fix: add cors for dev link

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ require("dotenv").config();
 
 const { PORT } = process.env;
 const app = express();
+const whitelist = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Jika origin ada di dalam whitelist, izinkan akses
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Mengizinkan pengiriman kredensial
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors(corsOptions));
 
 app.use("/", require("./routes/routes"));
 
